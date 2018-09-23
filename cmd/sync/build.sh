c9e1fd7f9231792f5692e56361c3a49e2bec3291
Denis Vashchuk <vashchuk.denis@gmail.com>
2018-09-23 18:12:49 +0300

    fix photoslibrary patching

--- cmd/sync/build.sh
+++ cmd/sync/build.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 source .env
-(cd $GOPATH/src/google.golang.org/api/photoslibrary/v1 && git apply $CI_PROJECT_DIR/patches/media-item-filename.patch)
 
 set -e 
 FLAGS="-ldflags \"-X main.GoogleClientId=$GOOGLE_CLIENT_ID -X main.GoogleClientSecret=$GOOGLE_CLIENT_SECRET\""
 
 mkdir -p builds
 go get
+(cd $GOPATH/src/google.golang.org/api/photoslibrary/v1 && git apply $CI_PROJECT_DIR/patches/media-item-filename.patch) || :
 GOOS=linux   GOARCH=386   go build $FLAGS -o builds/gphotosync_linux_386
 GOOS=linux   GOARCH=amd64 go build $FLAGS -o builds/gphotosync_linux_amd64
 GOOS=linux   GOARCH=arm   go build $FLAGS -o builds/gphotosync_linux_arm7

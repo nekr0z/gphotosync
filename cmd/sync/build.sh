70c894f43f38fd4ad796125d773fde91a292e7b6
Denis Vashchuk <vashchuk.denis@gmail.com>
2018-09-23 19:45:25 +0300

    fix build script

--- cmd/sync/build.sh
+++ cmd/sync/build.sh
@@ -1,15 +1,17 @@
-#!/bin/sh
+#!/bin/bash
 
-source .env
+source ./.env
 
-set -ex
-FLAGS="-ldflags \"-X main.GoogleClientId=$GOOGLE_CLIENT_ID -X main.GoogleClientSecret=$GOOGLE_CLIENT_SECRET\""
+set -e
+build() {
+    go build -ldflags "-X main.GoogleClientId=$GOOGLE_CLIENT_ID -X main.GoogleClientSecret=$GOOGLE_CLIENT_SECRET" -o builds/gphotosync-$GOOS-$GOARCH$BINEXT
+}
 
 mkdir -p builds
 go get
-GOOS=linux   GOARCH=386   go build $FLAGS -o builds/gphotosync_linux_386
-GOOS=linux   GOARCH=amd64 go build $FLAGS -o builds/gphotosync_linux_amd64
-GOOS=linux   GOARCH=arm   go build $FLAGS -o builds/gphotosync_linux_arm7
-GOOS=darwin  GOARCH=amd64 go build $FLAGS -o builds/gphotosync_mac_amd64
-GOOS=windows GOARCH=386   go build $FLAGS -o builds/gphotosync_windows_386.exe
-GOOS=windows GOARCH=amd64 go build $FLAGS -o builds/gphotosync_windows_amd64.exe
+GOOS=linux   GOARCH=386 build
+GOOS=linux   GOARCH=amd64 build
+GOOS=linux   GOARCH=arm   build
+GOOS=darwin  GOARCH=amd64 build
+BINEXT=".exe" GOOS=windows GOARCH=386   build
+BINEXT=".exe" GOOS=windows GOARCH=amd64 build

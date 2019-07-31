c20caec1e1d6069e0e234faecdb7ede2dd90a378
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2019-07-31 14:36:09 +0300

    wait before starting

--- main.go
+++ main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"math/rand"
+	"time"
 )
 
 var GoogleClientId string
 var GoogleClientSecret string
 
 func main() {
+	// need to sleep for random number of milliseconds so as not to overload Google API
+	// will sleep between 0 and 1 minute
+	rand.Seed(time.Now().UnixNano())
+	n := rand.Intn(60000)
+	fmt.Printf("Waiting for about %d seconds...\n", (n / 1000))
+	time.Sleep(time.Duration(n) * time.Millisecond)
+	fmt.Println("Ready to start now")
+
 	localLibArg := flag.String("lib", "", "local library path")
 	flag.Parse()
 

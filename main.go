617342d0babb87520a4ac1b22d40ce8610c37381
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2019-08-01 11:00:49 +0300

    use local credentials if available

--- main.go
+++ main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"flag"
+	"fmt"
 	"log"
+	"path"
 )
 
 var (
@@ -16,6 +18,20 @@ func main() {
 	flag.Parse()
 
 	lib := Library{*localLibArg}
+
+	// if .client_secret.json exists in local lib path, use those credentials
+	cred := credentials{
+		id:     "",
+		secret: "",
+	}
+	err := readSecret(path.Join(*localLibArg, ".client_secret.json"), &cred)
+	if err != nil {
+		fmt.Printf("couldn't read credentials from .client_secret.json: %s\n", err)
+	} else {
+		fmt.Println("using custom credentials")
+		GoogleClientId = cred.id
+		GoogleClientSecret = cred.secret
+	}
 
 	if err := lib.Sync(); err != nil {
 		log.Fatal(err)

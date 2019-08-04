c2032336844e5d079d9c9ec38f3063f0b0bcb600
Evgeny Kuznetsov <evgeny@kuznetsov.md>
2019-08-04 15:28:38 +0300

    require more accurate-looking timestamp

--- sync_test.go
+++ sync_test.go
@@ -8,7 +8,7 @@ import (
 func TestAddTimestampToPath(t *testing.T) {
 	testTime, _ := time.Parse("2006-01-02 15:04:05.000000000 MST -07:00", "1609-09-12 19:02:35.123456789 PDT +03:00")
 	got := addTimestampToPath("somepath/whatever.tex", testTime)
-	want := "somepath/whatever.tex-62359ca6994c1b15.tex"
+	want := "somepath/whatever-gphotosync-62359ca6994c1b15.tex"
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
